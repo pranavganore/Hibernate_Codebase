341f504acc562584be95f99b5395b48ef090f641
pranavganore <pranavganore@localhost>
2021-02-22 16:54:08 -0500

    Created Instructor and InstuctorDetail classes to map

---
+++ hb-01-one-to-one-uni/src/com/pbg/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,82 @@
+package com.pbg.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*----- Hibernate advanced mappings - One-to-One Mappings Demo -----*/
+
+//	Annotate the class as an entity and map to database table
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+	
+
+	
+	//	Define the fields
+	//	Annotate the fields with database column names
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	
+	//	Create constructors
+
+	public InstructorDetail() {
+		
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		super();
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	
+	//	Generate Getter/Setter methods
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	//	Generate toString() Method
+	
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+	
+	
+
+}
